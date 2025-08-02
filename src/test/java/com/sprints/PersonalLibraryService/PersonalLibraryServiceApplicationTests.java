c31f13b0bd7ff2a83d236330ac6808cf03957784
Tesla <alihossamtesla1@gmail.com>
2025-08-02 14:24:31 +0300

    Initial commit

---
+++ src/test/java/com/sprints/PersonalLibraryService/PersonalLibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprints.PersonalLibraryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalLibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
